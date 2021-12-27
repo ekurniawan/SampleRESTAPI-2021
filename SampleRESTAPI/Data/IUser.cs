11bc278c228bc64893fe61eab14d943fc9ee1d8e
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-12-27 09:49:14 +0700

    menambahkan registration

---
+++ SampleRESTAPI/Data/IUser.cs
@@ -0,0 +1,15 @@
+﻿using SampleRESTAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SampleRESTAPI.Data
+{
+    public interface IUser
+    {
+        Task Registration(User user);
+        Task AddRole(string rolename);
+        Task AddUserToRole(string username, string role);
+        Task<List<string>> GetRolesFromUser(string username);
+        Task<User> Authenticate(string username, string password);
+    }
+}
